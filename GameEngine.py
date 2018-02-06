00a763df2d73c8296fac4ff409f5d3974df4e3dc
Gavin Yu <zyu@wpi.edu>
2018-02-06 13:31:31 -0500

    game enginer works

--- GameEngine.py
+++ GameEngine.py
@@ -1,14 +1,19 @@
-import AIEngine
+import AIEngine as aie
 import os
 import time
 import Evals_Heuristics as eh
 """game engine, I/O"""
 
 hometeamName = "ggboys"
-guestteamName = raw_input("Who am I playing againt?\n")
+# initialize an empty board
+a_board = eh.initialize_board()
+# initialize engine
+minimax_engine = aie.Minimax(a_board)
 
+# scan the file
 def scanFile():
 	scanFile = [f for f in os.listdir('.') if os.path.isfile(f)]
+	# return a list fo scanned file
 	return scanFile
 
 def readMove():
@@ -19,13 +24,12 @@ def readMove():
 	player = 0
 	if(unparsedMoves[0] == hometeamName):
 		player = 1
-	if(unparsedMoves[0] == guestteamName):
+	else:
 		player = 2
 	x = letter2Int(unparsedMoves[1])
 	y = int(unparsedMoves[2]) - 1
-	move = eh.Stone(x, y, player)
-	print move
-	return move
+	a_board[x][y] = player
+
 
 def writeMove(colum, row):
 	x = Int2Letter(colum)
@@ -33,6 +37,7 @@ def writeMove(colum, row):
 	f = open("move_file", "w")
 	f.write(hometeamName+" "+ x + " "+ y)
 	f.close()
+	a_board[colum][row] = 1
 
 def letter2Int(letter):
 	letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O"]
@@ -52,17 +57,28 @@ def Int2Letter(number):
 
 
 def rungame():
-	files = scanFile()
-	for f in files:
-		if f == "end_game":
-			return
-		if f == guestteamName + ".go":
-			time.sleep(2)
-			print "===========waited for 2 secs ================"
-			rungame()
-		if f == hometeamName + ".go":
-			readMove()
-			writeMove(0, 0)
-			readMove()
+	# checker for the first move
+	gameround = 1
+	while True:
+		files = scanFile()
+		for f in files:
+			if f == "end_game":
+				return
+			if f == hometeamName + ".go":
+				if gameround == 1:
+					writeMove(7, 7)
+					gameround += 1
+					time.sleep(2)
+					continue
+				else:
+					readMove()
+					# decide next step and return the new_board
+					next_move = minimax_engine.minimax_decision(a_board, 1)
+					# get next step's stone by comparing the new_board and the old_board
+					next_stone = eh.get_step(a_board, next_move)
+					writeMove(next_stone.x, next_stone.y)
+					time.sleep(2)
+					continue
+	#time.sleep(2)
 
 rungame()
